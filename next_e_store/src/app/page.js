deb7f40b879c5158fcd994363b65142d27a2a282
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-09-12 11:22:11 +0200

    removed description and changed name to  title

--- next_e_store/src/app/page.js
+++ next_e_store/src/app/page.js
@@ -26,8 +26,7 @@ const HomePage = async () => {
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
+            <h2>{product.title}</h2>
             <p>${product.price}</p>
           </li>
         ))}
