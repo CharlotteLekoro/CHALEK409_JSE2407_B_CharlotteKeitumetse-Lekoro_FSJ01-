3faa74c11b0cb8c0991d88b39861f94203976d71
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-09-16 09:35:08 +0200

    refactored the tailwind.config

--- next_e_store/tailwind.config.js
+++ next_e_store/tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        nerko: ['"Nerko One"', "cursive"], // Adding the Nerko One font
+      },
+      animation: {
+        "bounce-1": "bounce 1s infinite 0.2s",
+        "bounce-2": "bounce 1s infinite 0.4s",
+        "bounce-3": "bounce 1s infinite 0.6s",
       },
     },
   },
